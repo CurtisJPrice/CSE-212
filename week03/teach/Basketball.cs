b71501de47656729f84f477d3317f83e31f63be9
Your Name <you@example.com>
2025-01-24 00:38:39 -0800

    w03

---
+++ week03/teach/Basketball.cs
@@ -0,0 +1,46 @@
+/*
+ * CSE 212 Lesson 6C 
+ * 
+ * This code will analyze the NBA basketball data and create a table showing
+ * the players with the top 10 career points.
+ * 
+ * Note about columns:
+ * - Player ID is in column 0
+ * - Points is in column 8
+ * 
+ * Each row represents the player's stats for a single season with a single team.
+ */
+
+using Microsoft.VisualBasic.FileIO;
+
+public class Basketball
+{
+    public static void Run()
+    {
+        var players = new Dictionary<string, int>();
+
+        using var reader = new TextFieldParser("../../../basketball.csv");
+        reader.TextFieldType = FieldType.Delimited;
+        reader.SetDelimiters(",");
+        reader.ReadFields(); // ignore header row
+        while (!reader.EndOfData) {
+            var fields = reader.ReadFields()!;
+            var playerId = fields[0];
+            var points = int.Parse(fields[8]);
+
+            if (!players.ContainsKey(playerId))
+                players[playerId] = 0;
+            players[playerId] += points;
+        }
+
+        Console.WriteLine($"Players: {{{string.Join(", ", players)}}}");
+
+        var topPlayers = new string[10];
+        var sortedPlayers = players.OrderByDescending(x => x.Value).ToArray();
+
+        Console.WriteLine("Top Players:");
+        for (var i = 0; i < 10; i++) {
+            Console.WriteLine($"{i+1}. {sortedPlayers[i].Key} - {sortedPlayers[i].Value}");
+        }
+    }
+}
---
+++ week03/teach/UniqueLetters.cs
@@ -0,0 +1,21 @@
+public static class UniqueLetters {
+    public static void Run() {
+        var test1 = "abcdefghjiklmnopqrstuvwxyz"; // Expect True because all letters unique
+        Console.WriteLine(AreUniqueLetters(test1));
+
+        var test2 = "abcdefghjiklanopqrstuvwxyz"; // Expect False because 'a' is repeated
+        Console.WriteLine(AreUniqueLetters(test2));
+
+        var test3 = "";
+        Console.WriteLine(AreUniqueLetters(test3)); // Expect True because its an empty string
+    }
+
+    /// <summary>Determine if there are any duplicate letters in the text provided</summary>
+    /// <param name="text">Text to check for duplicate letters</param>
+    /// <returns>true if all letters are unique, otherwise false</returns>
+    private static bool AreUniqueLetters(string text) {
+        // TODO Problem 1 - Replace the O(n^2) algorithm to use sets and O(n) efficiency
+        var unique = new HashSet<char>(text);
+        return text.Length == unique.Count;
+    }
+}
