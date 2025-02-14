ec57dfe5f56494be4b6fc9c745c9f4fff7c3d5db
Your Name <you@example.com>
2025-02-13 20:13:45 -0800

    fixes

---
+++ week06/code/Node.cs
@@ -0,0 +1,50 @@
+public class Node
+{
+    public int Data { get; set; }
+    public Node? Right { get; private set; }
+    public Node? Left { get; private set; }
+
+    public Node(int data)
+    {
+        this.Data = data;
+    }
+
+    public void Insert(int value)
+    {
+        if (value == Data)
+            return;
+
+        if (value < Data)
+        {
+            if (Left is null)
+                Left = new Node(value);
+            else
+                Left.Insert(value);
+        }
+        else
+        {
+            if (Right is null)
+                Right = new Node(value);
+            else
+                Right.Insert(value);
+        }
+    }
+
+    public bool Contains(int value)
+    {
+        if (value == Data)
+            return true;
+            
+        if (value < Data)
+            return Left != null && Left.Contains(value);
+        
+        return Right != null && Right.Contains(value);
+    }
+
+    public int GetHeight()
+    {
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
+    }
+}
